1e579052d90abaa6a85717f5209b0f6c46a2b4b8
Juan Navas <navascruzf@gmail.com>
2020-02-20 21:51:09 -0400

    updated styles, added email pattern

--- src/app/views/user/user-create/user-create.component.ts
+++ src/app/views/user/user-create/user-create.component.ts
@@ -1,40 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserService } from 'src/app/services/user.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-user-create',
-  templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.scss']
+  selector: "app-user-create",
+  templateUrl: "./user-create.component.html",
+  styleUrls: ["./user-create.component.scss"]
 })
 export class UserCreateComponent implements OnInit {
-
-  
   userForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    public firebaseService: UserService,
-  ) { }
+    public firebaseService: UserService
+  ) {}
 
   ngOnInit() {
     this.userForm = this.fb.group({
-      name: ['',[ Validators.required, Validators.pattern("[A-z' ']*"),] ],
-      surname: ['', [Validators.required, Validators.email]],
-      age: ['', [Validators.required,Validators.pattern("[1-9][0-9]")]]
+      name: ["", [Validators.required, Validators.pattern("[A-z' ']*")]],
+      surname: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
+          )
+        ]
+      ],
+      age: ["", [Validators.required, Validators.pattern("[1-9][0-9]")]]
     });
   }
 
-  onSubmit(value){
-    let avatarLink = "https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png";
-    this.firebaseService.createUser(value, avatarLink)
-    .then(
-      res => {
-        this.router.navigate(['/']);
-      }
-    )
+  onSubmit(value) {
+    let avatarLink =
+      "https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png";
+    this.firebaseService.createUser(value, avatarLink).then(res => {
+      this.router.navigate(["/"]);
+    });
   }
-
 }
--- src/app/views/user/user-edit/user-edit.component.ts
+++ src/app/views/user/user-edit/user-edit.component.ts
@@ -1,45 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { UserService } from 'src/app/services/user.service';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { UserService } from "src/app/services/user.service";
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-user-edit',
-  templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.scss']
+  selector: "app-user-edit",
+  templateUrl: "./user-edit.component.html",
+  styleUrls: ["./user-edit.component.scss"]
 })
 export class UserEditComponent implements OnInit {
   user: any;
   userForm: FormGroup;
 
-  constructor(private router: Router, private route: ActivatedRoute,
-    public firebaseService: UserService, 
-    private fb: FormBuilder) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public firebaseService: UserService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
-    
-    let iduser= this.route.snapshot.paramMap.get("id");
-    this.firebaseService.getUser(iduser)
-    .subscribe(result => {
+    let iduser = this.route.snapshot.paramMap.get("id");
+    this.firebaseService.getUser(iduser).subscribe(result => {
       this.user = result.payload.data();
       this.user.id = iduser;
       this.userForm = this.fb.group({
-        name: [this.user.name,[ Validators.required, Validators.pattern("[A-z' ']*"),] ],
-        surname: [this.user.surname, [Validators.required, Validators.email]],
-        age: [this.user.age, [Validators.required,Validators.pattern("[1-9][0-9]")]]
+        name: [
+          this.user.name,
+          [Validators.required, Validators.pattern("[A-z' ']*")]
+        ],
+        surname: [
+          this.user.surname,
+          [
+            Validators.required,
+            Validators.pattern(
+              "[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
+            )
+          ]
+        ],
+        age: [
+          this.user.age,
+          [Validators.required, Validators.pattern("[1-9][0-9]")]
+        ]
       });
-    })
-    
+    });
   }
 
-  onSubmit(value){
-    value.avatar=this.user.avatar;
-    this.firebaseService.updateUser(this.user.id, value)
-    .then(
-      res => {
-        this.router.navigate(['/']);
-      }
-    )
+  onSubmit(value) {
+    value.avatar = this.user.avatar;
+    this.firebaseService.updateUser(this.user.id, value).then(res => {
+      this.router.navigate(["/"]);
+    });
   }
-
 }
